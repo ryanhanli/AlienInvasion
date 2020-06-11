a1bfc4b1bbd7c7bc8263f1b932d5154c1cf0d2b8
Ryan Li <rl3294@nyu.edu>
2020-06-11 01:13:36 -0400

    Add bullet class and firing capability

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvasion:
 	"""Overall class to manage game assets and behavior."""
@@ -13,15 +14,21 @@ class AlienInvasion:
 		self.settings = Settings()
 		
 		self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+		# Full screen enable.  Note: issues will arise if you have multiple monitors 
+		#self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+		#self.settings.screen_width = self.screen.get_rect().width
+		#self.settings.screen_height = self.screen.get_rect().height
 		pygame.display.set_caption("Alien Invasion")
 		
 		self.ship = Ship(self)
+		self.bullets = pygame.sprite.Group()
 	
 	def run_game(self):
 		"""Start the main loop for the game."""
 		while True:
 			self._check_events()
 			self.ship.update()
+			self._update_bullets()
 			self._update_screen()
 			
 	def _check_events(self):
@@ -40,6 +47,10 @@ class AlienInvasion:
 			self.ship.moving_right = True
 		elif event.key == pygame.K_LEFT:
 			self.ship.moving_left = True
+		elif event.key == pygame.K_q:
+			sys.exit()
+		elif event.key == pygame.K_SPACE:
+			self._fire_bullet()
 	
 	def _check_keyup_events(self, event):
 		"""Respond to key releases."""
@@ -47,11 +58,32 @@ class AlienInvasion:
 			self.ship.moving_right = False
 		elif event.key == pygame.K_LEFT:
 			self.ship.moving_left = False
+			
+	def _fire_bullet(self):
+		"""Create a new bullet and add it to the bullets group."""
+		if len(self.bullets) < self.settings.bullets_allowed:
+			new_bullet = Bullet(self)
+			self.bullets.add(new_bullet)
+			
+	def _update_bullets(self):
+		"""Update position of bullets and get rid of old bullets."""
+		# Update bullet positions.
+		self.bullets.update()
+		
+		# Get rid of bullets that have disappeared.
+		# We use copy here because we can't delete a list we're looping through
+		for bullet in self.bullets.copy():
+			if bullet.rect.bottom <= 0:
+				self.bullets.remove(bullet)
+		# Takes more time to write output to the terminal than it does to draw graphics to the game window
+		#print(len(self.bullets))
 					
 	def _update_screen(self):
 		"""Update images on the screen, and flip to the new screen."""
 		self.screen.fill(self.settings.bg_color)
 		self.ship.blitme()
+		for bullet in self.bullets.sprites():
+			bullet.draw_bullet()
 		
 		pygame.display.flip()
 		
