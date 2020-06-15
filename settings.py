6d2a0cedfb08290e66fb65fc0cc914bca210f58c
Ryan Li <rl3294@nyu.edu>
2020-06-15 01:36:09 -0400

    Add lose condition upon ship reaching bottom, implement lives and game states

--- settings.py
+++ settings.py
@@ -14,7 +14,7 @@ class Settings:
 		
 		# Bullet settings
         self.bullet_speed = 1.5
-        self.bullet_width = 3000
+        self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 0, 0)
         self.bullets_allowed = 3
